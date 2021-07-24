fcf10cb4ebaa73b5bc1fceadc070d2ae3790b42c
Matthew Molloy <whamtet@gmail.com>
2021-07-24 10:05:38 +0900

    all stubs in place

--- src/jinjava_clj/core.clj
+++ src/jinjava_clj/core.clj
@@ -1,21 +1,23 @@
 (ns jinjava-clj.core
   (:require
-    [camel-snake-kebab.core :refer [->camelCase]])
+    [camel-snake-kebab.core :refer [->camelCase]]
+    [clojure.java.io :as io])
   (:import
     com.hubspot.jinjava.loader.ResourceLocator
     com.hubspot.jinjava.lib.tag.Tag
     com.hubspot.jinjava.lib.tag.IncludeTag
     com.hubspot.jinjava.lib.fn.ELFunctionDefinition
     com.hubspot.jinjava.Jinjava
+    java.io.File
     org.jinjava.CLJStatic))
 
 (def jinjava (Jinjava.))
 (def context (.getGlobalContext jinjava))
 
-(defn reify-tag [s]
+(defn reify-tag [[name end-name]]
   (reify Tag
-         (getName [this] s)
-         (getEndTagName [this])
+         (getName [this] name)
+         (getEndTagName [this] end-name)
          (interpret [this node interpreter] "")))
 
 (defn el-def [name args]
@@ -37,23 +39,37 @@
                     (as-> full-name $
                           (.replace $ "path=\"../" "")
                           (.replace $ "\"" "")
-                          (str "resources/korumsandbox/templates/" $)
+                          (str "korumsandbox/templates/" $)
+                          (io/resource $)
                           (slurp $)))))
 
-(doseq [t ["module" "form" "textarea" "require_css" "image_src" "menu"]]
+(doseq [t [["module"]
+           ["form"]
+           ["textarea"]
+           ["image_src"]
+           ["menu"]
+           ["icon"]
+           ["text"]
+           ["rich_text"]
+           ["dnd_area" "end_dnd_area"]
+           ["related_blog_posts"]]]
   (.registerTag context (reify-tag t)))
 (.registerTag context global-partial)
 
 (def functions {"require_css" [String]
                 "get_asset_url" [String]
-                "require_js" [String]})
+                "require_js" [String]
+                "blog_popular_posts" [String Long]
+                "blog_tags" [String Long]})
 (doseq [[name args] functions]
   (.registerFunction context (el-def name args)))
 
 (.setResourceLocator jinjava resource-locator)
 
-(println
-   "hi"
-   (.render jinjava
-            (slurp "resources/korumsandbox/templates/homepage.html")
-            {}))
+(def templates
+  '("korumsandbox/templates/blog-archive.html" "korumsandbox/templates/case-studies2.html" "korumsandbox/templates/team.html" "korumsandbox/templates/search_results.html" "korumsandbox/templates/homepage.html" "korumsandbox/templates/people.html" "korumsandbox/templates/axceller.html" "korumsandbox/templates/contact.html" "korumsandbox/templates/mls.html" "korumsandbox/templates/howwework.html" "korumsandbox/templates/our consultants customer.html" "korumsandbox/templates/case-studies.html" "korumsandbox/templates/404.html" "korumsandbox/templates/consultants copy.html" "korumsandbox/templates/our consultants.html" "korumsandbox/templates/landing-page.html" "korumsandbox/templates/consultants.html" "korumsandbox/templates/our-story.html" "korumsandbox/templates/pandt.html" "korumsandbox/templates/clients-home.html" "korumsandbox/templates/single-post.html" "korumsandbox/templates/locations.html"))
+(def template "korumsandbox/templates/homepage.html")
+
+(defn render-template [f]
+  (.render jinjava (-> f io/resource slurp) {}))
+
--- src/jinjava_clj/static.clj
+++ src/jinjava_clj/static.clj
@@ -3,8 +3,12 @@
     :name org.jinjava.CLJStatic
     :methods [^:static [requireCss [String] String]
               ^:static [getAssetUrl [String] String]
-              ^:static [requireJs [String] String]]))
+              ^:static [requireJs [String] String]
+              ^:static [blogPopularPosts [String Long] java.util.List]
+              ^:static [blogTags [String Long] java.util.List]]))
 
 (defn -requireCss [path] "")
 (defn -getAssetUrl [path] "")
 (defn -requireJs [path] "")
+(defn -blogPopularPosts [path limit] [])
+(defn -blogTags [blog-name limit] [])
